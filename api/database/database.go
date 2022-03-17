143a2c8973f5da4e2be3486f783f54076d323ba4
anshugupta673 <alch.anshu@gmail.com>
2022-03-17 14:21:57 +0530

    database file update

---
+++ api/database/database.go
@@ -0,0 +1,17 @@
+package database
+
+import(
+	"context"
+	"github.com/go-redis/redis/v8"
+	"os"
+)
+
+var Ctx = context.Background()
+
+func CreateClient(dbNo int) *redis.CLient {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("DB_ADDR")
+		Password: os.Getenv("DB_PASS")
+		DB: dbNo, 
+	})
+}
