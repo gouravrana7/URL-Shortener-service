6f095478b93450ab2e15f7a51d05fa02d1b8d9b1
anshugupta673 <alch.anshu@gmail.com>
2022-03-17 13:56:33 +0530

    check for valid URL

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -17,4 +17,22 @@ type response struct {
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
 }
- 
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	//implement rate limiting
+
+	//check if the input sent by the user if an actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid URL"})
+	}
+
+	//check for domain error
+
+	//enforce https, SSL
+}
