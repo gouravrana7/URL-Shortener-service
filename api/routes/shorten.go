b5b74e02560851376cc380c5cb368bd94a743002
anshu <anshu@pop-os.localdomain>
2022-05-02 01:33:19 +0530

    shorten.go rate limit reset and ip store

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,9 +1,16 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
-	"github.com/anshugupta673/URL-Shortener-service/api/helpers"
+	"github.com/anshugupta673/URL-Shortener-service/database"
+	helper "github.com/anshugupta673/URL-Shortener-service/helpers"
+	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 type request struct {
@@ -23,22 +30,90 @@ type response struct {
 func ShortenURL(c *fiber.Ctx) error {
 	body := new(request)
 
-	if err := c.BodyParser(&body); err != nil {
+	if err := c.BodyParser(&body); err != nil { // parse JSON in req to struct understood by golang
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
 	}
 
-	//implement rate limiting, here we will check the IP of the user who's calling the fun we check the IP search it in the database
-
-	//check if the input sent by the user if an actual URL
-	if !govalidator.IsURL(body.URL) {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid URL"})
+	// implement rate limiting
+	//implement rate limiting, here check the IP of the user who's calling the fun we check the IP search it in the database
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		// val, _ := r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "Rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
 	}
 
+	// check if input is actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	//check for domain error (user can user the localhost:3000 as a URL to shorten then our program may enter an infinite loop, we can implement a fun in helpaer package to check for domain error)
 	//check for domain error
-	if !helpers.RemoveDomainError(body.URL) {
-		return c.Status(fiber.StatusServiceUnavaliable).JSON()
+	if !helper.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": ""})
 	}
 
 	//enforce https, SSL
-	body.URL = helpers.EnforceHTTP(body.URL)
+	body.URL = helper.EnforceHTTP(body.URL)
+
+	var id string
+	if body.CustomShort == "" {
+		id = uuid.New().String()[0:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "URL already in use",
+		})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Unable to connect to server",
+		})
+	}
+
+	rsp := response{
+		URL:             body.URL,
+		CustomShort:     "",
+		Expiry:          body.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
+
+	r2.Decr(database.Ctx, c.IP())
+
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	rsp.XRateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	rsp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+
+	rsp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(rsp)
+
 }
